4aad943345346293aaca49b0e08e554bc3e9e1bb
이승재 <lsj9057@daum.net>
2025-02-16 11:28:14 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.09 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+
+        int[] dice = { a, b, c, d };
+        Arrays.sort(dice);
+
+        int answer = 0;
+
+        if (dice[0] == dice[3]) {
+            answer = 1111 * dice[3];
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            answer = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            answer = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+        } else if (dice[0] == dice[1]) {
+            answer = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            answer = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            answer = dice[0] * dice[1];
+        } else {
+            answer = dice[0];
+        }
+
+        return answer;
+    }
+}
