62479832faa00d91c4af76471ab2623176c17cdd
이승재 <lsj9057@daum.net>
2025-02-18 10:31:52 +0900

    [level 0] Title: 세로 읽기, Time: 15.52 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        for(int i = 0; i<my_string.length()/m; i++){
+            answer += "" + my_string.charAt(c-1);
+            c += m;
+        }
+        return answer;
+    }
+}
