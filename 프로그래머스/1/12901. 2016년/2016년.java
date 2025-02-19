1480c554021fc239874b1e016fe0462f2e8a7837
이승재 <lsj9057@daum.net>
2025-02-19 19:00:10 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,12 +1,12 @@
 class Solution {
     public String solution(int a, int b) {
         String[] answer = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
-        int result = -1;
+        int result = 0;
         int[] day = {31,29,31,30,31,30,31,31,30,31,30,31};
         for(int i = 0; i<a-1; i++){
             result += day[i];
         }
-        result += b;
+        result += (b-1);
         String y = answer[(result%7)];
         
         return y;
