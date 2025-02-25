3cc5aff8f7bb6f41e205adcef363092d289cb070
이승재 <lsj9057@daum.net>
2025-02-25 22:11:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 270.20 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 113 MB, 시간: 309.47 ms
+메모리: 95.4 MB, 시간: 270.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 25일 17:09:28
+2025년 02월 25일 22:11:07
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,21 +1,14 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
-        
-        Stack<String> stack = new Stack<>();
-        for(int i = 0; i< participant.length; i++){
-            stack.push(participant[i]);
-        }
-        
-        for(int i = completion.length-1; i>=0; i--){
-            if(stack.peek().equals(completion[i])){
-                stack.pop();
+        int i;
+        for (i=0; i<completion.length; i++){
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
             }
         }
-        
-        return stack.peek();
+        return participant[i];
     }
 }
