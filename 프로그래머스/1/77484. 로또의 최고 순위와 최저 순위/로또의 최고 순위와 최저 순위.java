bbb06d1103941b1ce96abb13c355ac8cf1aa93fa
이승재 <lsj9057@daum.net>
2025-03-10 17:45:22 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
         int zeroCount = 0;
-        int count = 6;
+        int count = 7;
         
         for(int i = 0; i < lottos.length; i++){
             if(lottos[i] == 0){
@@ -18,22 +18,22 @@ class Solution {
         }
         
         switch(zeroCount){
-            case 0: if(count == 6){
+            case 0: if(count == 7){
                 answer[0] = 6; answer[1] = 6;
                 }
                 else {
-                    answer[0] = count+1; answer[1] = count+1;
+                    answer[0] = count; answer[1] = count;
                 }
                 break;
-            case 1: answer[0] = Math.abs(count-zeroCount)+1; answer[1] = count+1;
+            case 1: answer[0] = Math.abs(count-zeroCount); answer[1] = count;
                 break;
-            case 2: answer[0] = Math.abs(count-zeroCount)+1; answer[1] = count+1;
+            case 2: answer[0] = Math.abs(count-zeroCount); answer[1] = count;
                 break;
-            case 3: answer[0] = Math.abs(count-zeroCount)+1; answer[1] = count+1;  
+            case 3: answer[0] = Math.abs(count-zeroCount); answer[1] = count;  
                 break;
-            case 4: answer[0] = Math.abs(count-zeroCount)+1; answer[1] = count+1;
+            case 4: answer[0] = Math.abs(count-zeroCount); answer[1] = count;
                 break;
-            case 5: answer[0] = Math.abs(count-zeroCount)+1; answer[1] = count+1; 
+            case 5: answer[0] = Math.abs(count-zeroCount); answer[1] = count; 
                 break;
             case 6: answer[0] = 1; answer[1] = 6;
                 break;
