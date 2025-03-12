b8af34c8e38609d4a66c35233b474be968dddaf1
이승재 <lsj9057@daum.net>
2025-03-12 15:37:07 +0900

    [Silver III] Title: 주유소, Time: 292 ms, Memory: 30508 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] oil = new int[N];
+        int[] load = new int[N-1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i< N-1; i++){
+            load[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i< N; i++){
+            oil[i] = Integer.parseInt(st.nextToken());
+        }
+        int min = oil[0];
+        int price = load[0] * min;
+
+        for(int i = 1; i< N-1; i++) {
+            if(min > oil[i]){
+                min = oil[i];
+            }
+            price += load[i] * min;
+        }
+        System.out.println(price);
+    }
+}
