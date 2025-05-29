be56338751796d9fe8b31ae1e6fd95e4033c6212
이승재 <lsj9057@daum.net>
2025-05-29 20:26:19 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45580 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> q = new LinkedList<>();
+
+        int N =  Integer.parseInt(br.readLine());
+
+        for(int i = 1; i<=N; i++){
+            q.add(i);
+        }
+
+        while(q.size()!=1){
+            q.poll();
+            q.addLast(q.poll());
+        }
+        System.out.println(q.peekFirst());
+    }
+}
