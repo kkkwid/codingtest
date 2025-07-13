6197684ae505c9018bf18214c263ae7fb438f9e9
이승재 <lsj9057@daum.net>
2025-07-14 00:20:20 +0900

    [Silver II] Title: N과 M (12), Time: 140 ms, Memory: 14552 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n, m;
+    public static int[] arr;
+    public static int[] nums;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st= new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        nums=new int[n];
+
+        st= new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            nums[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+    public static void dfs(int cnt, int start) {
+        if (cnt == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int before=0;
+        for (int i = start; i < n; i++) {
+            if(before!=nums[i]){
+                arr[cnt] = nums[i];
+                before=nums[i];
+                dfs(cnt + 1, i);
+            }
+        }
+    }
+}
