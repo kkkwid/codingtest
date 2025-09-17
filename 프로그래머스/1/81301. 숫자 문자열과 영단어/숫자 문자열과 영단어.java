85b2367f301e14625fa5866dc105484eea27ae23
이승재 <lsj9057@daum.net>
2025-09-17 17:50:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.52 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,58 +1,9 @@
-import java.util.Arrays;
 class Solution {
     public int solution(String s) {
-        String answer = "";
-        char[] ch = new char[s.length()];
-        for(int i = 0; i< ch.length; i++){
-            ch[i] = s.charAt(i);
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-        String str = "";
-        for(int i = 0; i< ch.length; i++){
-            str += ch[i];
-            if(str.equals("zero") || str.equals("0")){
-                answer += "0";
-                str = "";
-            }
-            else if(str.equals("one") || str.equals("1")){
-                answer += "1";
-                str = "";
-            }
-            else if(str.equals("two") || str.equals("2")){
-                answer += "2";
-                str = "";
-            }
-            else if(str.equals("three") || str.equals("3")){
-                answer += "3";
-                str = "";
-            }
-            else if(str.equals("four") || str.equals("4")){
-                answer += "4";
-                str = "";
-            }
-            else if(str.equals("five") || str.equals("5")){
-                answer += "5";
-                str = "";
-            }
-            else if(str.equals("six") || str.equals("6")){
-                answer += "6";
-                str = "";
-            }
-            else if(str.equals("seven") || str.equals("7")){
-                answer += "7";
-                str = "";
-            }
-            else if(str.equals("eight") || str.equals("8")){
-                answer += "8";
-                str = "";
-            }
-            else if(str.equals("nine") || str.equals("9")){
-                answer += "9";
-                str = "";
-            }
-        }
-        int result = Integer.parseInt(answer);
-        
-        return result;
+        return Integer.parseInt(s);
     }
-    
 }
