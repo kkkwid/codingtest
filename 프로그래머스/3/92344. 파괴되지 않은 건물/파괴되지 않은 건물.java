7ecaa565f73e88acb85754c71337ab6231af7825
이승재 <lsj9057@daum.net>
2025-10-22 23:31:43 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 45.10 ms, Memory: 216 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,25 +1,32 @@
 class Solution {
     public int solution(int[][] board, int[][] skill) {
-        int n = board.length, m = board[0].length;
-        int[][] acc = new int[n + 1][m + 1];
+        int n = board.length;
+        int m = board[0].length;
+        int[][] sum = new int[n + 1][m + 1];
+
         for (int[] s : skill) {
-            int val = (s[0] == 1 ? -s[5] : s[5]);
-            int r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4];
-            acc[r1][c1] += val;
-            acc[r1][c2 + 1] -= val;
-            acc[r2 + 1][c1] -= val;
-            acc[r2 + 1][c2 + 1] += val;
+            int type = s[0];
+            int r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4], degree = s[5];
+            int val = type == 1 ? -degree : degree;
+            sum[r1][c1] += val;
+            sum[r1][c2 + 1] -= val;
+            sum[r2 + 1][c1] -= val;
+            sum[r2 + 1][c2 + 1] += val;
         }
+
         for (int i = 0; i <= n; i++)
             for (int j = 1; j <= m; j++)
-                acc[i][j] += acc[i][j - 1];
+                sum[i][j] += sum[i][j - 1];
+
         for (int j = 0; j <= m; j++)
             for (int i = 1; i <= n; i++)
-                acc[i][j] += acc[i - 1][j];
-        int ans = 0;
+                sum[i][j] += sum[i - 1][j];
+
+        int answer = 0;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                if (board[i][j] + acc[i][j] > 0) ans++;
-        return ans;
+                if (board[i][j] + sum[i][j] > 0) answer++;
+
+        return answer;
     }
 }
