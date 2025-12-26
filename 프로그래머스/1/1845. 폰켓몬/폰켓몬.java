d216ed66a292a5e45257ddbdeff6fa304d2cfc42
이승재 <lsj9057@daum.net>
2025-12-27 01:34:50 +0900

    [level 1] Title: 폰켓몬, Time: 3.84 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,17 +1,14 @@
 import java.util.*;
+
 class Solution {
+    
     public int solution(int[] nums) {
-        int result = 0;
-        Set<Integer> set = new HashSet<> ();
-        for(int i = 0; i<nums.length; i++){
-            set.add(nums[i]);
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
         }
-        
-        if(nums.length/2 < set.size()){
-            result = nums.length/2;
-            return result;
-        }
-        
-        return set.size();
+        return Math.min(set.size(), nums.length/2);
     }
+    
 }
