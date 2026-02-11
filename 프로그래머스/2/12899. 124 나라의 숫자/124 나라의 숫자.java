c8d79fbffba80a7a6cb93606d433f822afef9e31
이승재 <lsj9057@daum.net>
2026-02-11 09:16:39 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+ 
+        while (n > 0) {
+            int num = n%3;
+            if (num == 0) {
+                answer.append(4); 
+                n = n/3 - 1;
+            }
+            else {
+                answer.append(num);
+                n = n/3;
+            }
+ 
+        }
+ 
+        return answer.reverse().toString();
+    }
+}
