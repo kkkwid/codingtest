f15fcc59cdc0658ed92318d8210c954a6b5b2fd7
이승재 <lsj9057@daum.net>
2026-03-17 07:54:55 +0900

    [Gold III] Title: 소수의 연속합, Time: 280 ms, Memory: 27460 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,45 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static boolean[] prime;
-    static ArrayList<Integer> primeList;
-
-    public static void main(String[] args) throws IOException {
-
-
+    static int n;
+    static boolean sosu[];
+    static ArrayList<Integer>list = new ArrayList<>();
+    public static void main(String[] args) throws  IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        prime = new boolean[N+1];
-        primeList = new ArrayList<>();
-
-        for (int i = 2; i*i <= N; i++) {
-            if (!prime[i]) {
-                for (int j = i*i; j <= N; j += i) {
-                    prime[j] = true;
+        
+        
+        n = Integer.parseInt(br.readLine());
+        sosu = new boolean[n+1];
+        eratones();
+        for(int i=2; i<=n; i++) {
+            if(!sosu[i]) {
+                list.add(i);
+            }
+        }
+        
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int ans = 0;
+        
+        while(true) {
+            if(sum == n) {
+                ans++;
+                sum-=list.get(start++);
+            }
+            else if (sum > n) {
+                sum-=list.get(start++);
+            }
+            else {
+                if(end == list.size()) {
+                    System.out.println(ans);
+                    break;
                 }
+                sum+=list.get(end++);
             }
         }
-
-        for (int i = 2; i <= N; i++) {
-            if (!prime[i]) {
-                primeList.add(i);
+        
+    }
+    public static void eratones() {
+        sosu[1] = true;
+        for(int i=2; i<=n; i++) {
+            for(int j=i*2; j<=n; j+=i) {
+                sosu[j] = true;
             }
         }
-
-        int start = 0, end = 0, sum = 0, cnt = 0;
-        while (true) {
-            if (sum >= N) sum -= primeList.get(start++);
-            else if (end == primeList.size()) break;
-            else sum += primeList.get(end++);
-
-            if (sum == N) cnt++;
-        }
-
-        System.out.println(cnt);
-
     }
 }
