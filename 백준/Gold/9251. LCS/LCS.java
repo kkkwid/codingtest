0ac63dbd4873acd305489aba2c1a5f37ed4a4dc8
이승재 <lsj9057@daum.net>
2026-04-13 08:03:54 +0900

    [Gold IV] Title: LCS, Time: 136 ms, Memory: 18448 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+
+        int[][] dp = new int[str1.length + 1][str2.length + 1];
+
+        for (int i = 1; i <= str1.length; i++) {
+            for (int j = 1; j <= str2.length; j++) {
+                if (str1[i-1] == str2[j-1]) { 
+                    dp[i][j] = dp[i - 1][j - 1] + 1; 
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+
+        bw.write(dp[str1.length][str2.length] + "");
+        bw.flush();
+        bw.close();
+
+    }
+}
